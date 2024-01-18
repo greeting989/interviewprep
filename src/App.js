cdade866c235a8e508595588f87a2b5f2e5636ae
ayushee <ashrivastava0812@gmail.com>
2024-01-18 21:57:44 +0530

    accordian day 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 
 import './App.css';
 import Star from './components/Star';
+import Accordion from './components/accordion/Accordion';
 
 function App() {
   return (
     <div className="App">
-      <h1>Star Rating Component</h1>
-      <Star/>
+       <span className='accodionTitle'>Day 2 : Accordion Component</span>
+      <Accordion
+        title="What is your return policy?"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Accordion
+        title="How do I track my order?"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Accordion
+        title="Can I purchase items again?"
+        content="
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        "
+      />
     </div>
   );
 }
